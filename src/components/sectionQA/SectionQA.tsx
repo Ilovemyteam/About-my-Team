1ccb45b9691ee4eb4a959e48e85e1dc6cdc0cb93
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-02-29 10:26:49 +0100

    title section QA

---
+++ src/components/sectionQA/SectionQA.tsx
@@ -0,0 +1,10 @@
+import { Section } from "../shared/Section";
+import { TitleSectionQA } from "./TitleSectionQA";
+
+export const SectionQA = () => {
+    return (
+        <Section>
+            <TitleSectionQA />
+        </Section>
+    )
+}
