349170a32a538cc35f6804852c04d17d30fa89cd
IraMira333 <irinat333@gmail.com>
2024-03-01 13:24:01 +0200

    Update FeedbackSwiper.tsx

--- src/components/FeedbackSection/FeedbackSwiper.tsx
+++ src/components/FeedbackSection/FeedbackSwiper.tsx
@@ -3,6 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 
 import "swiper/css";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
 import feedbacks from "./feedbackData.json";
 import Image from "next/image";
 
@@ -18,14 +20,16 @@ const FeedbackSwiper = () => {
     return (
         <div>
             <Swiper
-                navigation
-                pagination={{ type: "fraction" }}
                 modules={[Navigation, Pagination]}
+                navigation
+                pagination={{ clickable: true }}
+                spaceBetween={50}
+                slidesPerView={3}
             >
                 {feedbacks.map(
                     ({
                         image,
-                        video,
+
                         name,
                         type,
                         id,
@@ -34,24 +38,14 @@ const FeedbackSwiper = () => {
                         <SwiperSlide key={id}>
                             <div>
                                 <h4>{name}</h4>
-                                {video ? (
-                                    <iframe
-                                        src={video}
-                                        frameBorder="0"
-                                        allowFullScreen
-                                        width="50%"
-                                        height="auto"
-                                    />
-                                ) : (
-                                    <Image
-                                        src={image}
-                                        alt={name}
-                                        style={{
-                                            width: "100%",
-                                            height: "100%",
-                                        }}
-                                    />
-                                )}
+                                <Image
+                                    src={image}
+                                    alt={name}
+                                    style={{
+                                        width: "100%",
+                                        height: "100%",
+                                    }}
+                                />
                                 <p>{type}</p>
                                 <p>{discription}</p>
                             </div>
